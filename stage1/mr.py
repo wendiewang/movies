9b8d946c3479217b66a348c4f82926febf55d798
ccontrast <concurrentcontrast@gmail.com>
2012-07-11 18:05:43 -0700

    day1

---
+++ stage1/mr.py
@@ -0,0 +1,18 @@
+
+item_file = open("/ml-100k/u.item")
+
+for line in item_file:
+    line = item_file.readline()
+    print line
+
+# movie_info = open("u.item")
+# movie_info = read.line("u.item")
+
+# movie_data = {}
+
+# { 'id': , 'name': , 'release date': , 
+# def movie_details(argv)
+# 	return 
+
+# # 42|Clerks (1994)|01-Jan-1994||
+# http://us.imdb.com/M/title-exact?Clerks%20(1994)|0|0|0|0|0|1|0|0|0|0|0|0|0|0|0|0|0|0|0
